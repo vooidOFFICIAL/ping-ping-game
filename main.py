41487b1a25f7dd334f1af8bbcc172794ca7e8bf5
VOOID <vooid@web.de>
2023-11-22 13:00:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,41 +2,41 @@ import pygame
 import sys
 import time
 
-# Initialisierung von Pygame
+# Initialization of Pygame
 pygame.init()
 
-# Farben
-schwarz = (0, 0, 0)
-weiß = (255, 255, 255)
+# Colors
+black = (0, 0, 0)
+white = (255, 255, 255)
 
-# Bildschirmgröße
-breite, höhe = 1000, 600  # Vergrößerte Bildschirmgröße
-bildschirm = pygame.display.set_mode((breite, höhe))
-pygame.display.set_caption("Ping Pong Spiel")
+# Screen size
+width, height = 1000, 600  # Enlarged screen size
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Ping Pong Game")
 
-# Schläger
-schläger_breite, schläger_höhe = 15, 100
-schläger1 = pygame.Rect(50, höhe // 2 - schläger_höhe // 2, schläger_breite, schläger_höhe)
-schläger2 = pygame.Rect(breite - 50 - schläger_breite, höhe // 2 - schläger_höhe // 2, schläger_breite, schläger_höhe)
+# Paddles
+paddle_width, paddle_height = 15, 100
+paddle1 = pygame.Rect(50, height // 2 - paddle_height // 2, paddle_width, paddle_height)
+paddle2 = pygame.Rect(width - 50 - paddle_width, height // 2 - paddle_height // 2, paddle_width, paddle_height)
 
 # Ball
-ball = pygame.Rect(breite // 2 - 15, höhe // 2 - 15, 30, 30)
+ball = pygame.Rect(width // 2 - 15, height // 2 - 15, 30, 30)
 ball_speed = [11, 11]
 
-# Spielgeschwindigkeit
-geschwindigkeit = 9
+# Game speed
+speed = 9
 
-# Startzeit
-startzeit = time.time()
+# Start time
+start_time = time.time()
 
-# Zähler für gewonnene Spiele
-gewinnzähler_spieler1 = 0
-gewinnzähler_spieler2 = 0
+# Counter for won games
+win_counter_player1 = 0
+win_counter_player2 = 0
 
-# Variable für die Geschwindigkeitsverdoppelung
-geschwindigkeitsverdoppelung = False
+# Variable for speed doubling
+speed_doubling = False
 
-# Hauptspiel-Schleife
+# Main game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,67 +44,66 @@ while True:
             sys.exit()
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w] and schläger1.top > 0:
-        schläger1.y -= geschwindigkeit
-    if keys[pygame.K_s] and schläger1.bottom < höhe:
-        schläger1.y += geschwindigkeit
-    if keys[pygame.K_UP] and schläger2.top > 0:
-        schläger2.y -= geschwindigkeit
-    if keys[pygame.K_DOWN] and schläger2.bottom < höhe:
-        schläger2.y += geschwindigkeit
+    if keys[pygame.K_w] and paddle1.top > 0:
+        paddle1.y -= speed
+    if keys[pygame.K_s] and paddle1.bottom < height:
+        paddle1.y += speed
+    if keys[pygame.K_UP] and paddle2.top > 0:
+        paddle2.y -= speed
+    if keys[pygame.K_DOWN] and paddle2.bottom < height:
+        paddle2.y += speed
 
-    # Ballbewegung
+    # Ball movement
     ball.x += ball_speed[0]
     ball.y += ball_speed[1]
 
-    # Kollision mit den Wänden
-    if ball.top <= 0 or ball.bottom >= höhe:
+    # Collision with walls
+    if ball.top <= 0 or ball.bottom >= height:
         ball_speed[1] = -ball_speed[1]
 
-    # Kollision mit den Schlägern
-    if ball.colliderect(schläger1) or ball.colliderect(schläger2):
+    # Collision with paddles
+    if ball.colliderect(paddle1) or ball.colliderect(paddle2):
         ball_speed[0] = -ball_speed[0]
 
-    # Ball zurücksetzen, wenn er das Spielfeld verlässt
+    # Reset ball if it leaves the playing field
     if ball.left <= 0:
-        ball.x = breite // 2 - 15
-        ball.y = höhe // 2 - 15
-        print("Spieler 2 hat gewonnen!")
-        gewinnzähler_spieler2 += 1
-        if gewinnzähler_spieler2 == 3:
-            print("Spieler 2 hat insgesamt dreimal gewonnen. Spiel beendet!")
+        ball.x = width // 2 - 15
+        ball.y = height // 2 - 15
+        print("Player 2 has won!")
+        win_counter_player2 += 1
+        if win_counter_player2 == 3:
+            print("Player 2 has won three times in total. Game over!")
             pygame.quit()
             sys.exit()
 
-    elif ball.right >= breite:
-        ball.x = breite // 2 - 15
-        ball.y = höhe // 2 - 15
-        print("Spieler 1 hat gewonnen!")
-        gewinnzähler_spieler1 += 1
-        if gewinnzähler_spieler1 == 3:
-            print("Spieler 1 hat insgesamt dreimal gewonnen. Spiel beendet!")
+    elif ball.right >= width:
+        ball.x = width // 2 - 15
+        ball.y = height // 2 - 15
+        print("Player 1 has won!")
+        win_counter_player1 += 1
+        if win_counter_player1 == 3:
+            print("Player 1 has won three times in total. Game over!")
             pygame.quit()
             sys.exit()
 
-    # Zeit seit Spielstart
-    vergangene_zeit = time.time() - startzeit
+    # Time since the start of the game
+    elapsed_time = time.time() - start_time
 
-    # Geschwindigkeit erhöhen nach einer bestimmten Zeit
-    if vergangene_zeit > 10 and not geschwindigkeitsverdoppelung:
-        geschwindigkeit + 2
-        geschwindigkeitsverdoppelung = True
+    # Increase speed after a certain time
+    if elapsed_time > 10 and not speed_doubling:
+        speed + 2
+        speed_doubling = True
 
-    # Bildschirm leeren
-    bildschirm.fill(schwarz)
+    # Clear the screen
+    screen.fill(black)
 
-    # Schläger und Ball zeichnen
-    pygame.draw.rect(bildschirm, weiß, schläger1)
-    pygame.draw.rect(bildschirm, weiß, schläger2)
-    pygame.draw.ellipse(bildschirm, weiß, ball)
+    # Draw paddles and ball
+    pygame.draw.rect(screen, white, paddle1)
+    pygame.draw.rect(screen, white, paddle2)
+    pygame.draw.ellipse(screen, white, ball)
 
-    # Bildschirm aktualisieren
+    # Update the screen
     pygame.display.update()
 
-    # Framerate einstellen
+    # Set the frame rate
     pygame.time.Clock().tick(30)
-
